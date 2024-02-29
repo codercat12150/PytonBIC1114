bc2a8758f3845b6d5665570593ba29ff912372b5
Tasfiah Calysta <38335774+codercat12150@users.noreply.github.com>
2024-02-29 14:39:29 +0800

    corrected code with updated output
    2
    [5, 2, 5, 0, -2, 1]
    11
    Minimum:  -2
    Maximum:  5

--- list29Feb2024.py
+++ list29Feb2024.py
@@ -1,6 +1,8 @@
 list1 = [1,-2,0, 5, 2, 5]
 print(list1.count(5))
-list2 = list1.reverse()
+list1.reverse()
+list2 = list1
 print(list2)
 print(sum(list1))
 print("Minimum: ", min(list1))
+print("Maximum: ", max(list1))
